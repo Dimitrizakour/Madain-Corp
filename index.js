ec9a2f8bdec55fd26f645ab3d13dccf730ef659e
Dimitrizakour <102598358+Dimitrizakour@users.noreply.github.com>
2022-04-06 03:29:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function filter(c) {
   var x, i;
   x = document.getElementsByClassName("categoryspan");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  
   for (i = 0; i < x.length; i++) {
     AddClass(x[i], "hide");
     if (x[i].className.indexOf(c) > -1) RemoveClass(x[i], "hide");
